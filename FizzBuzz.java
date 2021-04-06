b45de1004366e4be825b336537536a560a59e6b5
JagdeepMann <jagmann1101@gmail.com>
2021-04-06 15:12:51 -0500

    FizzBuzz Java Version

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public String fizzbuzz(Integer number) {
+        
+        if(number%5 == 0 && number%3 == 0){
+            return ("FizzBuzz");
+        }
+        else if(number%5 == 0){
+            return ("Buzz");
+        }
+        else if(number%3 == 0){
+            return ("Fizz");
+        }
+        else{
+            return Integer.toString (number);
+        }
+    }
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        
+        FizzBuzz fb = new FizzBuzz();
+        String str = fb.fizzbuzz(5);
+        System.out.println(str);
+    }
+}
